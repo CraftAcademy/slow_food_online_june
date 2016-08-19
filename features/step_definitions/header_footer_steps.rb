40be1450d079388c700cb087c881ceb8cca38e71
Lucia De Mojà <luciademoja@gmail.com>
2016-08-19 10:27:02 +0200

    tests header and adds header partial to application.html.haml

--- features/step_definitions/header_footer_steps.rb
+++ features/step_definitions/header_footer_steps.rb
@@ -1,11 +1,7 @@
-Given(/^I am on the "([^"]*)" page$/) do |arg1|
-  expect(page.current_path).to eq root_path
+Given(/^I am on the "([^"]*)" page$/) do |path|
+  visit root_path
 end
 
 Then(/^I should see a "([^"]*)" link$/) do |link|
   click_link_or_button link
-end
-
-Then(/^I should see "([^"]*)"$/) do |content|
-  expect(page).to have_content content
 end
